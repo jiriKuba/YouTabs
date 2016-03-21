8981c56b5b33232e13bb2bb420f7fa856d8b465d
Jiří Kuba <kuba.jirka90@gmail.com>
2016-03-21 18:22:55 +0100

    initial commit

---
+++ background.js
@@ -0,0 +1,27 @@
+
+var isReady = false;
+var bandSongText = "";
+chrome.runtime.onMessage.addListener(function(message,sender,sendResponse){
+  if(message.method == 'askServerRequest'){
+    if(typeof message.userData != 'undefined' && message.userData !== null){
+      bandSongText = message.userData;
+      sendServerRequest(bandSongText);
+    }  	
+  	sendResponse({ data: bandSongText});
+  }  
+  else if(message.method == 'getLastRequest'){
+    sendResponse({ data: bandSongText});
+  }  
+});
+
+function sendServerRequest(bandSongText) {
+  var tabsCall = "http://www.911tabs.com/search.php?search="+bandSongText;
+  chrome.tabs.create({url: tabsCall});
+}
+
+function documentReady(){
+  isReady = true;
+}
+
+//window.onload = documentReady;
+document.addEventListener('DOMContentLoaded', documentReady, false);
---
+++ popup.js
@@ -0,0 +1,70 @@
+var bandSongName = "";
+function loadBandSongName(callBack){
+
+	tryGetLastRequest();
+
+	tryGetYoutubeTitle(function(textData){
+      if(typeof textData != 'undefined' && textData!= "")
+      	bandSongName = textData;
+      
+      if(typeof callBack !='undefined'){
+  			callBack();
+  		}
+    });
+}
+
+function searchBtnClick(){
+	bandSongName = getSearchText();
+
+	chrome.runtime.sendMessage({method: "askServerRequest", userData: bandSongName}, function(response) {
+  		if(typeof response !='undefined' && typeof response.data !='undefined'){
+    		setSearchText(response.data);
+  		}
+	});
+}
+
+function tryGetYoutubeTitle(callback){
+
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+      var callRefer = callback;
+      chrome.tabs.sendMessage(tabs[0].id, {action: "getBandAndSong"}, function(response){
+
+        if(typeof callRefer != 'undefined'){
+          callRefer(response.data);
+        }
+      });  
+    });
+
+}
+
+function tryGetLastRequest(){
+	chrome.runtime.sendMessage({method: "getLastRequest"}, function(response) {
+  		if(typeof response !='undefined' && typeof response.data !='undefined'){
+    		setSearchText(response.data);
+  		}
+	});
+}
+
+function getSearchText(){
+	return document.getElementById('search-textbox').value;
+}
+
+function setSearchText(text){
+	bandSongName = text;
+	document.getElementById('search-textbox').value = text;
+}
+
+function documentReady(){
+	var searchBtn = document.getElementById('search-btn');
+	if(searchBtn !== null)
+	{
+		searchBtn.addEventListener('click', searchBtnClick, false);
+	}
+
+	loadBandSongName(function(){
+		setSearchText(bandSongName);
+	});
+}
+
+//window.onload = documentReady;
+document.addEventListener('DOMContentLoaded', documentReady, false);
